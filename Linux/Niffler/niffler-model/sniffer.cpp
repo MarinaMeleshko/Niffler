690a7257224dcb39c25eb0eceefb7dbdaa38db7f
MarinaMeleshko <irmelen909@gmail.com>
2019-05-05 00:53:13 +0300

    Sniffer: process packets

--- Linux/Niffler/niffler-model/sniffer.cpp
+++ Linux/Niffler/niffler-model/sniffer.cpp
@@ -62,15 +62,70 @@ void Sniffer::savePacket(BasePacket *packet){
 }
 
 BasePacket *processPPPoE(const u_char *buffer, const pcap_pkthdr *header){
+    struct iphdr *iph = (struct iphdr*)(buffer + 22);
+    switch (iph->protocol) {
+    case 6: {
+        TCPPacket* packet = new TCPPacket(buffer, header, 22);
+        return packet;
+    }
+    case 17: {
+        UDPPacket* packet = new UDPPacket(buffer, header, 22);
+        return packet;
+    }
+    default: {
+        BasePacket* packet = new BasePacket(buffer);
+        return packet;
+    }
+    }
 }
 
 BasePacket *processIP(const u_char *buffer, const pcap_pkthdr *header){
+    struct iphdr *iph = (struct iphdr*)(buffer + 14);
+    BasePacket* packet;
+    switch (iph->protocol) {
+    case 6: {
+        packet = new TCPPacket(buffer, header, 14);
+        return packet;
+    }
+    case 17: {
+        packet = new UDPPacket(buffer, header, 14);
+        return packet;
+    }
+    default: {
+        packet = new BasePacket(buffer);
+        return packet;
+    }
+    }
 }
 
 void Sniffer::processPacket(u_char *args, const pcap_pkthdr *header, const u_char *buffer){
+    (void)args;
+    (void)header;
+
+    struct ethhdr *ethh = (struct ethhdr*)(buffer);
+
+    switch (ethh->h_proto) {
+    case PPPOE_SESSION_PROTOCOL_CODE:
+        emit PacketProcessed(processPPPoE(buffer, header));
+        break;
+    case IP_PROTOCOL_CODE:
+        emit PacketProcessed(processIP(buffer, header));
+        break;
+    case ARP_PROTOCOL_CODE:
+        emit PacketProcessed(new ARPPacket(buffer, header));
+        break;
+    default:
+        break;
+    }
 }
 
 QString Sniffer::GetPacketParsedData(int id){
+    QList<BasePacket>::iterator i;
+    for (i = packetList->begin(); i != packetList->end(); ++i){
+        if ((*i).id == id){
+            return (*i).parsedData;
+        }
+    }
 }
 
 void Sniffer::run(){
