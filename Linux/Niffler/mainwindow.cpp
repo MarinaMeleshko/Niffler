75991f88f1d7de7d36bb45f5988704a5a1451983
MarinaMeleshko <irmelen909@gmail.com>
2019-05-06 17:31:01 +0300

    Pull packets method

--- Linux/Niffler/mainwindow.cpp
+++ Linux/Niffler/mainwindow.cpp
@@ -15,7 +15,22 @@ MainWindow::~MainWindow(){
 }
 
 void MainWindow::PullPackets(QList<BasePacket> *packets){
+    ui -> packetsTable -> clear();
+    ui -> packetsTable -> setRowCount(0);
 
+    int rowCounter = 0;
+    QList<BasePacket>::iterator i;
+    for (i = packets -> begin(); i!= packets -> end(); ++i, ++rowCounter){
+        char num_buffer [50];
+        sprintf(num_buffer, "%d", i -> id);
+        QString str = QString::fromUtf8(num_buffer);
+
+        ui -> packetsTable -> insertRow(rowCounter);
+        ui -> packetsTable -> setItem(rowCounter, 0, new QTableWidgetItem(str));
+        ui -> packetsTable -> setItem(rowCounter, 1, new QTableWidgetItem(i -> source));
+        ui -> packetsTable -> setItem(rowCounter, 2, new QTableWidgetItem(i -> destination));
+        ui -> packetsTable -> setItem(rowCounter, 3, new QTableWidgetItem(i -> protocol));
+    }
 }
 
 void MainWindow::AddPacket(BasePacket *packet){
